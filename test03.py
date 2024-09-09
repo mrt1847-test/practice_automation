0198f0e041b4e7ee49902b7a82a97aecdad05b9d
mrt1847-test <cease2504@naver.com>
2024-09-09 13:12:15 +0900

    Add files via upload

---
+++ test03.py
@@ -0,0 +1,17 @@
+"""
+정수 배열 numlist가 매개변수로 주어질 때,
+numlist를 오름차순으로 배열하는 함수를 만들어 주세요
+
+입출력 예
+numlist	                             result
+[9, 3, 5, 8, 1, 4, 2, 7, 6]	         [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+"""
+
+def solution(numlist):
+    answer = []
+    return answer
+
+k= []
+value = solution(k)
+print(value)
---
+++ test04.py
@@ -0,0 +1,15 @@
+"""
+배열에서 중복되는 요소를 제거하는 함수를 만들어 보세요
+
+numlist	                             result
+["a", "a", "b", "c"]	            ["a", "b", "c"]
+
+"""
+
+def solution(numlist):
+    answer = []
+    return answer
+
+k= []
+value = solution(k)
+print(value)
