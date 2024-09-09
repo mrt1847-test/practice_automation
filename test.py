ab4be058387aba13607c49a111df40638af30832
mrt1847-test <cease2504@naver.com>
2024-09-09 21:18:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,28 +3,22 @@ import os
 
 from appium import webdriver
 import time
+from appium.options.android import UiAutomator2Options
 
 # 디바이스 및 앱 정보 설정pip
-desired_caps = {
-    "platformName": "Android",
-    "deviceName": "AOS14",
-    "automationName": "UiAutomator2",
-    "app": "C:\\APK\\GmarketMobile-debugFinal.09090314.apk",
-    "newCommandTimeout": 900,
-    "appPackage": "com.ebay.kr.gmarket",
-    "appActivity": "com.ebay.kr.gmarket.eBayKoreaGmarketActivity",
-    "acceptInsecureCerts": true,
-    "noReset": "false",
-    "chromedriverExecutable": "c:\\webdriver\\appium\\chromedriver.exe",
-    "appium:chrome_options": {"androidPackage": "com.ebay.kr.gmarket"}
 
-}
+options = UiAutomator2Options()
+options.platformName = "Android"
+options.deviceName = "AOS14"  # 에뮬레이터 또는 실제 장치의 이름
+options.app = "C:\\APK\\GmarketMobile-debugFinal.09090314.apk"  # 앱의 APK 파일 경로
+options.appPackage = "com.ebay.kr.gmarket"  # 앱 패키지 이름
+options.appActivity = "com.ebay.kr.gmarket.eBayKoreaGmarketActivity"  # 시작 액티비티 이름
 
 # Appium 서버와 연결
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver = webdriver.Remote('http://localhost:4723', options=options)
 
 # 앱에서 자동화 테스트 수행
-time.sleep(5)  # 앱이 로드되기를 기다림
+time.sleep(100)  # 앱이 로드되기를 기다림
 
 # 예: 특정 버튼 클릭
 button = driver.find_element_by_id("com.example:id/button")
