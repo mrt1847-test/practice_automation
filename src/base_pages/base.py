e0255e165ef97ed01b0f9eec4a5d07da3420a6da
mrt1847-test <cease2504@naver.com>
2024-09-10 10:33:55 +0900

    Update base.py

--- src/base_pages/base.py
+++ src/base_pages/base.py
@@ -2,6 +2,44 @@ from typing import Tuple
 
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
+import os
+import subprocess
+import io
+import re
+
+import sys
+import json
+import time
+import numpy 														# pip install numpy
+import random
+import requests
+import datetime
+from datetime import datetime, timedelta
+
+
+# SELENIUM WEBDRIVER 제어 모듈 										# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.remote.command import Command
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.alert import Alert
+
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import *
+
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.ie.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.common.mobileby import MobileBy
 
 
 class BasePage:
