1aaaece9fc2b0b251eb5385d09e5647b348fd446
mrt1847 <122250753+mrt1847@users.noreply.github.com>
2024-12-31 10:34:31 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -15,30 +15,20 @@ def driver():
     # 디바이스 및 앱 정보 설정pip
     os_version = platform.platform()
     if 'Windows' in os_version:  # windows인 경우
-        options = UiAutomator2Options()
         app_path = os.path.abspath(config["win"]["app_path"])
         chrome_path = os.path.abspath(config["win"]["chrome_path"])
-        options.PlatformName = "Android"
-        options.deviceName = "AOS14"  # 에뮬레이터 또는 실제 장치의 이름
-        options.app = app_path  # 앱의 APK 파일 경로
-        options.appPackage = "com.ebay.kr.gmarket"  # 앱 패키지 이름
-        options.appActivity = "com.ebay.kr.gmarket.eBayKoreaGmarketActivity"  # 시작 액티비티 이름
-        options.adbExecTimeout = 60000
-        options.chromedriverExecutable = chrome_path
-        options.noReset = "false"
-
     elif 'mac' in os_version:
-        options = UiAutomator2Options()
         app_path = os.path.abspath(config["mac"]["app_path"])
         chrome_path = os.path.abspath(config["mac"]["chrome_path"])
-        options.platformName = "Android"
-        options.deviceName = "AOS14"  # 에뮬레이터 또는 실제 장치의 이름
-        options.app = app_path  # 앱의 APK 파일 경로
-        options.appPackage = "com.ebay.kr.gmarket"  # 앱 패키지 이름
-        options.appActivity = "com.ebay.kr.gmarket.eBayKoreaGmarketActivity"  # 시작 액티비티 이름
-        options.adbExecTimeout = 60000
-        options.chromedriverExecutable = chrome_path
-        options.noReset = "false"
+    options = UiAutomator2Options()
+    options.PlatformName = "Android"
+    options.deviceName = "AOS14"  # 에뮬레이터 또는 실제 장치의 이름
+    options.app = app_path  # 앱의 APK 파일 경로
+    options.appPackage = "com.ebay.kr.gmarket"  # 앱 패키지 이름
+    options.appActivity = "com.ebay.kr.gmarket.eBayKoreaGmarketActivity"  # 시작 액티비티 이름
+    options.adbExecTimeout = 60000
+    options.chromedriverExecutable = chrome_path
+    options.noReset = "false"
 
     # Appium 서버와 연결
     driver = None
